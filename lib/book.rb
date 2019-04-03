1f0586697db247be2ba0eea69aa7a8de79d89ec2
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-04-03 12:53:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,8 +12,24 @@ class Book
     @author = author 
   end 
   
-  # def 
-    
-  # end 
+   def author 
+    @author
+   end 
+  
+  def page_count= (num)
+    @page_count = num
+  end 
+  
+  def page_count 
+    @page_count
+  end 
+  
+  def genre= (genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
   
 end 
